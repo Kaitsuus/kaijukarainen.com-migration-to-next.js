d1f6577f27efa7c35cca9e59dc38a9cb3ed4a41a
Kai Jukarainen <kai.jukarainen@gmail.com>
2023-05-12 21:03:17 +0300

    Add reuseable navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
 "use client"
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../utils/i18n';
-import DevNavbar from '@/dev/Navbar';
 import DevHome from '@/dev/Home';
 import DevAbout from '@/dev/About'
 import DevSkills from '@/dev/Skills'
 import DevWork from '@/dev/Work'
 import Footer from '../components/Footer'
+import Navbar from '@/components/Navbar';
 
+const navItems = [
+  { to: "about", label: "About" },
+  { to: "skills", label: "Skills" },
+  { to: "work", label: "Work" },
+  { to: "footer", label: "Contact" },
+];
 
 export default function Home() {
   return (
     <div>
       <I18nextProvider i18n={i18n}>
-        <DevNavbar />
+        <Navbar navItems={navItems}/>
         <DevHome />
         <DevAbout />
         <DevSkills />
