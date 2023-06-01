02f6849984ebe336d3b824108d2945c1d711d9e8
Your Name <you@example.com>
2023-06-01 18:06:47 +0300

    screen

--- wordpress/Info-2.tsx
+++ wordpress/Info-2.tsx
@@ -12,14 +12,14 @@ const Info2: React.FC = () => {
   const { t } = useTranslation();
   const bgStyle: React.CSSProperties = {
     backgroundImage: `url(${backgroundImage.src})`,
-    backgroundSize: 'contain',
+    backgroundSize: 'cover',
     backgroundPosition: 'center',
   };
   return (
     <div
       id="info2"
       style={bgStyle}
-      className="h-full md:h-screen text-[#e8e7e7] z-10 flex-col justify-center "
+      className="h-screen sm:h-full w-full text-[#e8e7e7] z-10 flex flex-col justify-center"
     >
       {/* container */}
       <div className="hidden w-full bg-[#191a1e] p-4 md:flex mx-auto flex-col justify-center">
@@ -56,14 +56,14 @@ const Info2: React.FC = () => {
           </Link>
         </div>
 
-      <div className="md:hidden w-full bg-[#191a1e] p-4 flex mx-auto flex-col justify-center">
+      <div className="md:hidden w-full pb-5 bg-[#191a1e] p-4 flex mx-auto flex-col justify-center">
         <div className="flex flex-row mt-5">
-          <div className="pl-20 pr-5 flex flex-col">
-            <h1 className="text-4xl sm:text-4xl font-bold text-[#fafafa]">
+          <div className=" flex flex-col">
+            <h1 className="text-2xl font-bold text-[#fafafa]">
               {t('wpMainTextHeader')}
             </h1>
-            <div className="text-xl py-4">
-              <p className="text-xl text-[#fafafa] truncate whitespace-pre-wrap max-w-[1000px]">
+            <div className="text-sm py-4">
+              <p className="text-sm text-[#fafafa] truncate whitespace-pre-wrap max-w-[1000px]">
                 {t('wpMainText')}
                 <br />
                 <br />
