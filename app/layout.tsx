26aa723ea4d9e0c787a32f75d5c2a864b5aa94ab
Your Name <you@example.com>
2023-06-01 20:49:48 +0300

    add Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,60 +1,81 @@
-import './globals.css';
-import Head from 'next/head';
+import React from 'react';
+import { NextSeo } from 'next-seo';
 import { Inter } from 'next/font/google';
-import ogImageUrl from '../public/assets/wordpressverkkosivut.png'
+import Head from 'next/head';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
 
 export const metadata = {
+  openGraph: {
+    title: 'WordPress verkkosivut yrityksille.',
+    description:
+      'Tarvitseeko yrityksesi tyylikkäät ja käyttäjäystävälliset WordPress-verkkosivut, jotka houkuttelevat uusia asiakkaita ja kasvattavat myyntiäsi?',
+    url: 'https://kaijukarainen.com',
+    siteName: 'Kaijukarainen.com',
+    images: [
+      {
+        url: 'https://kaijukarainen.com/wordpressverkkosivut.png',
+        width: 1640,
+        height: 924,
+        alt: 'WordPress verkkosivut',
+      },
+    ],
+    locale: 'fi_FI',
+    type: 'website',
+  },
   title: 'WordPress verkkosivut yrityksille.',
   description:
     'Tarvitseeko yrityksesi tyylikkäät ja käyttäjäystävälliset WordPress-verkkosivut, jotka houkuttelevat uusia asiakkaita ja kasvattavat myyntiäsi?',
   keywords:
-    'WordPress, Woocommerce, software developer, developer, web designer, frontend developement, wordpress developement, full stack developement, web developement, ohjelmisto kehittäjä, verkkosivu toteutukset',
+    'WordPress, Woocommerce, verkkosivut, SEO, web designer, wordpress developement, web developement, ohjelmisto kehittäjä, verkkosivu toteutukset',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const ogImageUrlString = ogImageUrl.toString();
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="fi">
+    <>
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
         <meta name="author" content="Kai Jukarainen" />
-
-        {/* Open Graph tags */}
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={ogImageUrlString}/>
-        <meta property="og:url" content="https://kaijukarainen.com" />
-        <meta property="og:type" content="website" />
-        
         <link rel="canonical" href="https://kaijukarainen.com" />
 
-        <meta name="robots" content="index, follow" />
-        <meta name="googlebot" content="index, follow" />
-        <meta name="bingbot" content="index, follow" />
-        <meta name="referrer" content="no-referrer-when-downgrade" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-
-        <title>{metadata.title}</title>
-        {/* Add any additional CSS or JavaScript files */}
-        {/* Add font preloading or asynchronous loading code if necessary */}
+        <NextSeo
+          title={metadata.title}
+          description={metadata.description}
+          openGraph={{
+            url: 'https://kaijukarainen.com',
+            title: 'WordPress verkkosivut yrityksille.',
+            description:
+              'Tarvitseeko yrityksesi tyylikkäät ja käyttäjäystävälliset WordPress-verkkosivut, jotka houkuttelevat uusia asiakkaita ja kasvattavat myyntiäsi?',
+            images: [
+              {
+                url: 'https://kaijukarainen.com/wordpressverkkosivut.png',
+                width: 1640,
+                height: 924,
+                alt: 'WordPress verkkosivut',
+              },
+            ],
+            site_name: 'Kaijukarainen.com',
+            locale: 'fi_FI',
+            type: 'website',
+          }}
+        />
       </Head>
-      <body>
-        <main className={inter.className}>{children}</main>
-      </body>
-    </html>
+      <html lang="fi">
+        <body>
+          <main className={inter.className}>{children}</main>
+          <script type="application/ld+json">
+            {/* Add structured data here */}
+          </script>
+        </body>
+      </html>
+    </>
   );
 }
