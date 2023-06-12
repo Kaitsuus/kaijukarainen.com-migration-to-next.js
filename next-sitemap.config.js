39caebc6dcf333b2d1d87d1d4a88274fc557cc0c
Kaitsuus <kai.jukarainen@gmail.com>
2023-06-12 11:46:38 +0300

    refactor sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,26 @@
-const SITE_URL = process.env.SITE_URL || 'https://kaijukarainen.com';
+// next-sitemap.config.js
+const siteUrl = process.env.SITE_URL || 'https://kaijukarainen.com'
+const dynamicRoutes = process.env.dynamicRoutes || [{"page":"/","query":{}},{"page":"/dev","query":{}},{"page":"/privacy","query":{}},{"page":"/wordpressverkkosivut","query":{}}];
 
-/** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: SITE_URL,
+  siteUrl,
+  exclude: ['/404'],
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: ['/404'],
+      },
+      { userAgent: '*', allow: '/' },
+    ],
+    additionalSitemaps: [`${siteUrl}/server-sitemap.xml`],
+  },
+  // Include the dynamic routes in the sitemap
+  dynamicSitemap: {
+    // Pass the dynamic routes from next.config.js
+    routes: dynamicRoutes,
+  },
 };
 
-export default config;
+module.exports = config;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
 require('dotenv').config();
-const nextConfig = {
-  /* config options here */
+
+const dynamicRoutes = JSON.parse(process.env.dynamicRoutes || '[]');
+
+const config = {
+  generateStaticParams: async () => {
+    const paths = {
+      '/': { page: '/' },
+      '/dev': { page: '/dev' },
+      '/privacy': { page: '/privacy' },
+      '/wordpressverkkosivut': { page: '/wordpressverkkosivut' },
+    };
+
+    dynamicRoutes.forEach(route => {
+      paths[route.page] = {
+        page: route.page,
+        query: route.query,
+      };
+    });
+
+    return paths;
+  },
 };
 
-module.exports = nextConfig;
+module.exports = config;
